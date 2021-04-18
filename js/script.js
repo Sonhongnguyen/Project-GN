cb2a85eaa0dfb36b6dcb01faa18be7e21e73c56e
Sonhongnguyen <son6101999@gmail.com>
2021-04-18 21:15:26 +0700

    done

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*
+jQuery(document).ready(function($) {
+            var $filter = $('.top-bar');
+            var $filterSpacer = $('<div />', {
+                "class": "vnkings-spacer",
+                "height": $filter.outerHeight()
+            });
+            if ($filter.size())
+            {
+                $(window).scroll(function ()
+                {
+                    if (!$filter.hasClass('fix') && $(window).scrollTop() > $filter.offset().top)
+                    {
+                        $filter.before($filterSpacer);
+                        $filter.addClass("fix");
+                    }
+                    else if ($filter.hasClass('fix')  && $(window).scrollTop() < $filterSpacer.offset().top)
+                    {
+                        $filter.removeClass("fix");
+                        $filterSpacer.remove();
+                    }
+                });
+            }
+ 
+});
+*/
+var header = document.querySelector('.top-bar');
+var origOffsetY = header.offsetTop;
+
+function onScroll(e) {
+window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+header.classList.remove('sticky');
+}
+document.addEventListener('scroll', onScroll);
+
+// back to top
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
